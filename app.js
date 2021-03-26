ef9201aecf98f04619308ed795f78ab9cb7b16b3
sakshijain009 <62885857+sakshijain009@users.noreply.github.com>
2021-03-26 14:20:17 +0530

    basics

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mysql = require("mysql");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
