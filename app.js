3b0c19f73891307b660d2db664ea5dc1bdc6ffc7
sakshijain009 <62885857+sakshijain009@users.noreply.github.com>
2021-04-05 21:04:25 +0530

    login trying still with fail

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const session = require("express-session");
 const ejs = require("ejs");
 const mysql = require("mysql");
 const dotenv = require("dotenv");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 dotenv.config({path:'./.env'});
 const app = express();
@@ -13,8 +16,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static("public"));
 app.use(express.json());
-
-
+app.use(cookieParser());
+app.use(session({secret: "Your secret key"}));
 
 
 
@@ -77,7 +80,9 @@ app.get("/hospitaldata",(req,res) =>{
 });
 
 app.get("/hosp_login", (req,res) => {
-  res.render('hosp_login');
+  res.render('hosp_login',{
+    message:''
+  });
 });
 
 app.get("/inventory_login", (req,res) => {
@@ -169,12 +174,56 @@ app.post("/Registerhospital",(req,res)=>{
       }); 
 
 
-  });
-
-   
-       
+  });       
   
 });
+
+
+
+//Hospital login page post request
+app.post('/hospital_login',async(req,res)=>{
+
+    try{
+      console.log(req.body);
+      const email = req.body.hospid;
+      const pwd = req.body.hospwd;
+      con.query('SELECT * from hospital WHERE h_email = ?',[email],async(err,results)=>{
+          console.log(results);
+          if (!results || !(await bcrypt.compare(pwd,results[0].h_pwd))) {
+            res.status(401).render("hosp_login",{
+              message:'Error: Account not found.'
+            });
+          }else{
+            const id = results[0].H_id;
+
+            const token = jwt.sign({id:id},process.env.JWT_SECRET,{
+              expiresIn: process.env.JWT_EXPIRES_IN
+            });
+
+            console.log("The token is : "+token);
+
+            const cookieOptions = {
+              expires: new Date(
+                  Date.now() + process.env.JWT_COOKIE_EXPIRES*24*60*60*1000 //Converting to milli second
+                ),
+              httpOnly:true
+            }
+
+            res.cookie('jwt',token,cookieOptions);
+            res.status(200).redirect("/");
+          }
+      });
+
+
+
+    }catch(error){
+        console.log(error);
+    }
+});
+
+
+
+
 
 
 app.post("/Registerinventory",(req,res)=>{
