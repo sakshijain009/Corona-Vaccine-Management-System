2ae66fa99c49e9d9680d2c5c9fa99de78d53a993
esha123802 <76272081+esha123802@users.noreply.github.com>
2021-04-11 19:29:59 +0530

    added statistics with percentage

--- app.js
+++ app.js
@@ -67,22 +67,31 @@ app.get("/patient", (req, res) => {
 });
 
 app.get("/stat", (req, res) => {
-  let sql="SELECT count(P_Gender) as count, P_Gender FROM person GROUP By P_Gender";
+  let sql="SELECT count(P_Gender) as count, ((count(P_Gender)*100)/(select count(*) from person)) as percentage, P_Gender FROM person GROUP By P_Gender";
   let sqd = "SELECT count(*) as count from doctor"; 
-  let sqli= "SELECT count(*) as count ,H_type FROM hospital GROUP By H_type";
-  let doctor,type;
+  let sqli= "SELECT count(*) as count ,((count(H_type)*100)/(select count(*) from hosp_data)) as percentage,H_type FROM hosp_data GROUP By H_type;";
+  let sqlii = "select h_vac, count(*) as count, ((count(h_vac)*100)/(select count(*) from vacc_data)) as percentage from vacc_data group by h_vac;";
+  let sqlb = "select (select count(*) from vaccinates where Date_first is not NULL and Date_second = '0000-00-00') as onedose, (select count(*) from vaccinates where Date_first is not NULL and Date_second is not null) as twodose, (select count(*) from vaccinates where Date_first is NULL and Date_second is null) as nodose from dual;";
+  let doctor,type,vacc,dose;
   con.start.query(sqli,(err,result)=>{
     if (err) throw error;
     type=result;
-    console.log(type);
   }); 
   con.start.query(sqd,(err,result)=>{
     if (err) throw error;
     doctor=result[0];
   }); 
+  con.start.query(sqlii,(err,result)=>{
+    if (err) throw error;
+    vacc=result;
+  });
+  con.start.query(sqlb,(err,result)=>{
+    if (err) throw error;
+    dose=result[0];
+  }); 
   con.start.query(sql,(err,result)=>{
       if (err) throw error;
-    res.render("stat", {gender:result,doctor:doctor, type:type});
+    res.render("stat", {gender:result, doctor:doctor, type:type, vacc:vacc, dose: dose});
       });
 });
 
